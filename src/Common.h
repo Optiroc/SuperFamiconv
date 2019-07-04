de401501f5021239a8f180958cf3c5968a726550
David Lindecrantz <optiroc@gmail.com>
2019-07-04 23:35:13 +0200

    Bump version

--- src/Common.h
+++ src/Common.h
@@ -36,7 +36,7 @@ typedef std::vector<rgba_set_t> rgba_set_vec_t;
 
 namespace sfc {
 
-constexpr const char* VERSION = "0.8.4";
+constexpr const char* VERSION = "0.8.5";
 
 constexpr const char* COPYRIGHT = "Copyright (c) 2017-2019 David Lindecrantz";
 
